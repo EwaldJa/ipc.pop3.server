8070b45b3ef99209dd12c5ad94ceff6447ed8f9c
EwaldJa <45970173+EwaldJa@users.noreply.github.com>
2020-02-11 13:29:57 +0100

    initial commit

---
+++ src/test/java/ipc/pop3/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ipc.pop3.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
